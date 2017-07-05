d716739a74ce9067b8f9e3036a531ea60bd43989
Changning Niu <osu0082@owens-rw01.ten.osc.edu>
2017-07-05 19:54:51 -0400

    correct chem.py

--- mc2/chem.py
+++ mc2/chem.py
@@ -7,16 +7,15 @@ with open('data_all', 'r') as f:
         print 'This is not a binary alloy!\n'
         quit()
     e1, e2 = [float(x) for x in items[1:3]]
-    n1a, n2a = int(items[4]), int(items[6])
+    n1a, n2a = float(items[4]), float(items[6])
     for i in range(1, len(lines)):
         items = lines[i].split()
         e1_old, e2_old = e1, e2
         e1, e2 = [float(x) for x in items[1:3]]
         n1a_old, n2a_old = n1a, n2a
-        n1a, n1b, n2a, n2b = [int(x) for x in items[4:8]]
+        n1a, n1b, n2a, n2b = [float(x) for x in items[4:8]]
         n1, n2 = n1a + n1b, n2a + n2b
-        if n1a - n1a_old == 0:
-            continue
+        if n1a == n1a_old: continue
         mu1a = e1 / n1 + n1b / n1 * (e1 - e1_old) / (n1a - n1a_old)
         mu1b = e1 / n1 - n1a / n1 * (e1 - e1_old) / (n1a - n1a_old)
         mu2a = e2 / n2 + n2b / n2 * (e2 - e2_old) / (n2a - n2a_old)
